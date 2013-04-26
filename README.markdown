0c7ea602717606790ed78bad403ab0a5b53f02aa
ShiraNai7 <shiranai7@hotmail.com>
2013-04-26 16:43:01 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,334 @@
+# jQuery ScrollWatch 1.2
+
+jQuery plugin to implement navigation or any other functionality based on current scrolling position of the page or inside a custom element.
+
+## Browser support
+
+Tested in Firefox, Google Chrome, Safari, Opera and MSIE 7+
+
+## Usage
+
+The plugin provides two jQuery methods you can use.
+
+### `$(sectionSelector).scrollWatch(options);`
+
+This is the "main" method. It attaches scrollwatch to the given scroller (`window` by default) and matches sections according to current scrolling position.
+
+It is up to you to implement what is going to happen.
+
+<pre>$(window).load(function () {
+
+        var scrollWatch = $('div.section').scrollWatch({
+            callback: function (focus) {
+                // do something based on current focus
+            }
+            // more options here
+        });
+
+    });
+    </pre>
+
+### `$(menuSelector).scrollWatchMenu(sectionSelector, options);`
+
+This method makes it easy to show active section in a navigation menu.
+
+<pre>$(window).load(function () {
+
+        var scrollWatch = $('ul#menu').scrollWatchMenu('div.section', {
+            // options here
+        });
+
+    });
+    </pre>
+
+## Return value
+
+Both `$(selector).scrollWatch(options);` and `$(menuSelector).scrollWatchMenu(sectionSelector, options);` return `false` on failure or an object with following properties:
+
+*   **update** - function to recalculate section boundaries and update current focus
+*   **unbind** - function to unbind and disable the scrollwatch instance
+*   **pause** - function to suspend focus update
+*   **resume** - function to resume focus update
+
+### Notes
+
+*   .update() function should be called on the scrollwatch instance if size of contents in the scroller element (usually entire page) change dynamically (ajax-loaded contents, open/close features etc.)
+*   if applied to specific element (instead of window), that element should have *position: relative* or *absolute*
+
+## Options
+
+<table>
+  <thead>
+    <tr>
+      <th>
+        Name
+      </th>
+      
+      <th>
+        Default
+      </th>
+      
+      <th>
+        Description
+      </th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <tr>
+      <th>
+        scroller
+      </th>
+      
+      <td>
+        window
+      </td>
+      
+      <td>
+        element to watch for scrolling events
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        resolutionMode
+      </th>
+      <td>
+        0
+      </td>
+      
+      <td>
+        Determines how the active section is chosen <ul>
+          <li>
+            <strong></strong> - section that is occupying the largest portion of the view is chosen
+          </li>
+          <li>
+            <strong>1</strong> - section that is directly in intersection or nearest to the focus line is chosen
+          </li>
+        </ul>
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        callback
+      </th>
+      
+      <td>
+        required
+      </td>
+      
+      <td>
+        Callback invoked when the focus is updated. It is passed three arguments: <ol>
+          <li>
+            object with following properties (or array of those objects if multiMode = true): <ul>
+              <li>
+                <strong>index</strong> - section number (0 based)
+              </li>
+              <li>
+                <strong>focusHeight</strong> - null or height of the section's visible area
+              </li>
+              <li>
+                <strong>focusIntersection</strong> - null or an array with top coordinate as element 0 and bottom coordinate as element 1
+              </li>
+              <li>
+                <strong>elem</strong> - DOM element of the section
+              </li>
+              <li>
+                <strong>isFull</strong> - indicates that the section is entirely in the view area
+              </li>
+              <li>
+                <strong>asClosest</strong> - indicates that the section was chosen as closest (because no section was directly in the view)
+              </li>
+            </ul>
+          </li>
+          
+          <li>
+            top coordinate of the view
+          </li>
+          <li>
+            bottom coordinate of the view
+          </li>
+        </ol>
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        focusRatio
+      </th>
+      
+      <td>
+        0.3819..
+      </td>
+      
+      <td>
+        Percentage of the view height that determines position of the focus line.
+      </td>
+    </tr>
+
+    <tr>
+      <th>
+        debugFocusLine
+      </th>
+      
+      <td>
+        false
+      </td>
+      
+      <td>
+        Display position of the focus line (for debugging purposes).
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        topDownWeight
+      </th>
+      
+      <td>
+	   0
+      </td>
+      
+      <td>
+        Extra focus height added to the section if it preceedes the other (used in resolutionMode 0 only).
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        viewMarginTop
+      </th>
+      
+      <td>
+       0
+      </td>
+      
+      <td>
+        Height of an area at the top of the view to be excluded (e.g. navigation menu with position: fixed).
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        viewMarginBottom
+      </th>
+      
+      <td>
+       0
+      </td>
+      
+      <td>
+        Height of an area at the bottom of the view to be excluded (e.g. navigation menu with position: fixed).
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        multiMode
+      </th>
+      
+      <td>
+        false
+      </td>
+      
+      <td>
+        Enabling this turns 'focus' argument of callback into an array and no resolution is performed.
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+### Menu options
+
+These options are active only when `$(menuSelector).scrollWatchMenu(sectionSelector, options);` is used.
+
+<table>
+  <thead>
+    <tr>
+      <th>
+        Name
+      </th>
+      
+      <th>
+        Default
+      </th>
+      
+      <th>
+        Description
+      </th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <tr>
+      <th>
+        menuActiveClass
+      </th>
+      
+      <td>
+        active
+      </td>
+      
+      <td>
+        Class of the active menu item.
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        menuItemSelector
+      </th>
+      
+      <td>
+        *
+      </td>
+      
+      <td>
+        Selector to match menu items, <code>*</code> matches all direct children of the menu element.
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        menuScrollOffset
+      </th>
+      
+      <td>
+        0
+      </td>
+      
+      <td>
+        Scroll offset applied when a menu link containing a hash is clicked.
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        menuScrollSpeed
+      </th>
+      
+      <td>
+        500
+      </td>
+      
+      <td>
+        Scroll animation speed, <code></code> to disable.
+      </td>
+    </tr>
+    
+    <tr>
+      <th>
+        menuInitialHashOffsetTolerance
+      </th>
+      
+      <td>
+        40
+      </td>
+      
+      <td>
+        Maximum scroll position difference to apply the <code>menuScrollOffset</code> after page load.
+      </td>
+    </tr>
+  </tbody>
+</table>
---
+++ jquery.scrollwatch.js
@@ -0,0 +1,482 @@
+/**
+ * ScrollWatch 1.2 / jQuery plugin
+ * Support: all modern browsers and MSIE 7+
+ * @author ShiraNai7 <shira.cz>
+ */
+void function ($) {
+
+    "use_strict";
+
+    var isWebkit = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
+            || /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+
+    /**
+     * Get Y position of DOM element, relative to given offset parent
+     * @param elem DOM element
+     * @param offsetParent DOM element or null
+     * @return integer
+     */
+    function getElementY(elem, offsetParent)
+    {
+        // do not check parent if window
+        if (window === offsetParent || undefined === offsetParent) {
+            offsetParent = null;
+        }
+
+        // determine position
+        var y = 0;
+        do y += elem.offsetTop;
+        while ((elem = elem.offsetParent) && elem !== offsetParent);
+
+        // return
+        return y;
+    }
+
+    /**
+     * Get absolute Y position of DOM element
+     * @param elem DOM element
+     * @return integer
+     */
+    function getElementYAbs(elem)
+    {
+        var y = 0;
+        do y += elem.offsetTop;
+        while((elem = elem.offsetParent));
+
+        return y;
+    }
+
+    /**
+     * Get intersection of two inervals
+     * Requirements: aLeft < aRight, bLeft < bRight
+     *
+     * @param aLeft
+     * @param aRight
+     * @param bLeft
+     * @param bRight
+     * @return array|null [left, right] or null
+     */
+    function getIntersection(aLeft, aRight, bLeft, bRight)
+    {
+        if (bLeft > aRight || bRight < aLeft) {
+            // no intersection
+            return null;
+        }
+        return [
+            aLeft > bLeft ? aLeft : bLeft,
+            aRight > bRight ? bRight : aRight
+        ];
+    }
+
+    /**
+     * Apply ScrollWatch to given elements
+     *
+     * @param options
+     * @return object|bool
+     */
+    $.fn.scrollWatch = function (options) {
+
+        options = $.extend({}, $.fn.scrollWatch.defaults, options);
+
+        if (typeof options.callback !== 'function') {
+            throw new Error('The "callback" option is required and must be a function');
+        }
+
+        // prepare
+        var
+            elems = this,
+            sections,
+            scroller = $(options.scroller),
+            scrollerIsWindow = (window === options.scroller),
+            scrollerVisibleHeight,
+            scrollerFullHeight,
+            debugFocusLine,
+            paused = false
+        ;
+
+        // abort if no elements
+        if (0 === elems.length) {
+            return false;
+        }
+
+        /**
+         * Compute section boundaries
+         */
+        var sectionEnd;
+        function computeSectionBoundaries()
+        {
+            sections = [];
+            elems.each(function (){
+                var elementY = getElementY(this, options.scroller)
+                sectionEnd = elementY + this.offsetHeight;
+                sections.push([elementY, sectionEnd, sectionEnd - elementY]);
+            });
+            sections.sort(function (a, b) { return a[0] - b[0]; });
+        }
+
+        /**
+         * Compute scroller height
+         */
+        function computeScrollerHeight()
+        {
+            if (scrollerIsWindow) {
+                //scrollerHeight = $(document.body).height();
+                scrollerVisibleHeight = $(document.body).height();
+                scrollerFullHeight = document.getElementsByTagName('html')[0].scrollHeight;
+            } else {
+                scrollerVisibleHeight = options.scroller.clientHeight;
+                scrollerFullHeight = options.scroller.scrollHeight;
+            }
+        }
+
+        /**
+         * Update focus
+         */
+        function updateFocus()
+        {
+            if (paused) {
+                return;
+            }
+
+            // determine current view
+            var
+                viewTop = scroller.scrollTop(),
+                //viewBottom = viewTop + scroller.height()
+                viewBottom = viewTop + scrollerVisibleHeight
+            ;
+
+            // apply view margins
+            if (0 !== options.viewMarginTop) {
+                viewTop += options.viewMarginTop;
+            }
+            if (0 !== options.viewMarginBottom) {
+                viewBottom = Math.max(viewTop + 1, viewBottom - options.viewMarginBottom);
+            }
+
+            // collect focus candidates
+            var focusCandidates = [], focusIntersection, focusHeight;
+            //console.log(viewTop, viewBottom, scrollerVisibleHeight, scrollerFullHeight);
+            if (scrollerFullHeight - viewBottom < 5) {
+
+                // always choose last section if the view is near the end
+                var lastSection = sections.length - 1;
+                focusIntersection = getIntersection(viewTop, viewBottom, sections[lastSection][0], sections[lastSection][1]);
+                focusCandidates.push({
+                    index: lastSection,
+                    focusIntersection: focusIntersection,
+                    focusHeight: (null === focusIntersection) ? null : (focusIntersection[1] - focusIntersection[0]),
+                    elem: elems[lastSection],
+                    isFull: focusHeight >= sections[lastSection][2],
+                    asClosest: false
+                });
+
+            } else if (viewTop - options.viewMarginTop < 5) {
+
+                // always choose first section if the view is near the beginning
+                focusIntersection = getIntersection(viewTop, viewBottom, sections[0][0], sections[0][1]);
+                focusCandidates.push({
+                    index: 0,
+                    focusIntersection: focusIntersection,
+                    focusHeight: (null === focusIntersection) ? null : (focusIntersection[1] - focusIntersection[0]),
+                    elem: elems[0],
+                    isFull: focusHeight >= sections[0][2],
+                    asClosest: false
+                });
+
+            } else {
+
+                // determine using intersections
+                for (var i = 0; i < sections.length; ++i) {
+                    focusIntersection = getIntersection(viewTop, viewBottom, sections[i][0], sections[i][1]);
+                    if (null !== focusIntersection) {
+                        focusCandidates.push({
+                            index: i,
+                            focusIntersection: focusIntersection,
+                            focusHeight: focusIntersection[1] - focusIntersection[0],
+                            elem: elems[i],
+                            isFull: focusHeight >= sections[i][2],
+                            asClosest: false
+                        });
+                    }
+                }
+
+                // use section closest to the top of the view if no intersection was found
+                if (0 === focusCandidates.length) {
+
+                    var sectionClosest = null, sectionOffsetTop;
+                    for (i = 0; i < sections.length; ++i) {
+                        sectionOffsetTop = Math.abs(sections[i][0] - viewTop);
+                        if (null === sectionClosest || sectionClosest[1] > sectionOffsetTop) {
+                            sectionClosest = [i, sectionOffsetTop];
+                        }
+                    }
+
+                    focusCandidates.push({
+                        index: sectionClosest[0],
+                        focusIntersection: null,
+                        focusHeight: null,
+                        elem: elems[sectionClosest[0]],
+                        isFull: false,
+                        asClosest: true
+                    });
+
+                }
+
+            }
+
+            // process candidates
+            if (!options.multiMode) {
+
+                if (1 === focusCandidates.length) {
+
+                    // single candidate available
+                    options.callback(focusCandidates[0], viewTop, viewBottom);
+
+                } else {
+
+                    // multiple candidate resolution
+                    var finalFocus = null;
+                    switch (options.resolutionMode) {
+
+                        // choose using focus height
+                        case 0:
+                            focusCandidates.sort(function (a, b) {
+                                if (a.index < b.index) return b.focusHeight - a.focusHeight - options.topDownWeight;
+                                return b.focusHeight - a.focusHeight;
+                            });
+                            finalFocus = focusCandidates[0];
+                            break;
+
+                        // choose using intersection or distance from the focus line
+                        case 1:
+
+                            var viewFocusLineOffset = viewTop + (viewBottom - viewTop) * options.focusRatio;
+                            
+                            if (options.debugFocusLine) {
+                                debugFocusLine.style.top = Math.round(viewFocusLineOffset) + 'px';
+                            }
+
+                            // find direct intersection with the focus line
+                            for (i = 0; i < focusCandidates.length; ++i) {
+                                if (focusCandidates[i].focusIntersection[0] <= viewFocusLineOffset && focusCandidates[i].focusIntersection[1] >= viewFocusLineOffset) {
+                                    finalFocus = focusCandidates[i];
+                                    break;
+                                }
+                            }
+
+                            // find nearest candidate if no direct intersection exists
+                            if (null === finalFocus) {
+                                for (i = 0; i < focusCandidates.length; ++i) {
+                                    focusCandidates[i].focusRatioOffsetDistance = Math.min(
+                                        Math.abs(focusCandidates[i].focusIntersection[0] - viewFocusLineOffset),
+                                        Math.abs(focusCandidates[i].focusIntersection[1] - viewFocusLineOffset)
+                                    );
+                                }
+                                focusCandidates.sort(function (a, b){
+                                    return a.focusRatioOffsetDistance - b.focusRatioOffsetDistance;
+                                });
+                                finalFocus = focusCandidates[0];
+                            }
+                            break;
+
+                        // invalid
+                        default:
+                            throw new Error('Invalid resolution mode');
+
+                    }
+
+                    // use the chosen focus
+                    options.callback(finalFocus, viewTop, viewBottom);
+
+                }
+
+            } else {
+
+                // all candidates (multi mode)
+                options.callback(focusCandidates, viewTop, viewBottom);
+
+            }
+        }
+
+        /**
+         * Update all
+         */
+        function updateAll()
+        {
+            computeSectionBoundaries();
+            computeScrollerHeight();
+            updateFocus();
+        }
+        
+        // create focus ratio debug line
+        if (options.debugFocusLine && 1 === options.resolutionMode) {
+            debugFocusLine = document.createElement('div');
+            debugFocusLine.style.width = '100%';
+            debugFocusLine.style.height = '0';
+            debugFocusLine.style.position = 'absolute';
+            debugFocusLine.style.left = '0';
+            debugFocusLine.style.top = '0';
+            debugFocusLine.style.borderTop = '1px solid yellow';
+            debugFocusLine.style.borderBottom = '1px solid red';
+            debugFocusLine.style.zIndex = '9999';
+            debugFocusLine = $(debugFocusLine).appendTo(scrollerIsWindow ? document.body : scroller)[0];
+        }
+
+        // initial update
+        updateAll();
+
+        // update focus when scroller view changes
+        scroller.scroll(updateFocus);
+
+        // update all when scroller dimensions change (window only)
+        if (scrollerIsWindow) {
+            scroller.resize(updateAll);
+        }
+
+        // return controller object
+        return {
+            unbind: function () {
+                scroller.unbind('scroll', updateFocus);
+                if (scrollerIsWindow) {
+                    scroller.unbind('resize', updateAll);
+                }
+            },
+            update: function () {
+                updateAll();
+            },
+            pause: function () {
+                paused = true;
+            },
+            resume: function () {
+                paused = false;
+                updateFocus();
+            }
+        };
+
+    };
+
+    /**
+     * Apply ScrollWatch to given elements and reflect it into simple menu
+     *
+     * @param sections section selector or jQuery object
+     * @param options
+     * @return object|bool
+     */
+    $.fn.scrollWatchMenu = function(sections, options) {
+        var
+            items,
+            activeItem = null,
+            scroller = $(isWebkit ? document.body : 'html'),
+            scrollWatch
+        ;
+
+        // compose options
+        options = $.extend({}, $.fn.scrollWatch.defaults, $.fn.scrollWatchMenu.defaults, options, {
+            callback: function (focus) {
+                if (focus.index < items.length && focus.index !== activeItem) {
+                    if (null !== activeItem) {
+                        $(items[activeItem]).removeClass(options.menuActiveClass);
+                    }
+                    $(items[focus.index]).addClass(options.menuActiveClass);
+                    activeItem = focus.index;
+                }
+            }
+        });
+
+        // find items
+        items = $(this)[('*' === options.menuItemSelector) ? 'children' : 'find'](options.menuItemSelector);
+
+        // initial hash
+        if (0 !== options.menuScrollOffset && window.location.hash) {
+            $('a', items).each(function () {
+                if (this.hash === window.location.hash) {
+                    var
+                        hash = this.hash.substr(1),
+                        target = $('#' + hash + ', ' + '[name=' + hash + ']')
+                    ;
+                    if (target.length > 0) {
+                        var targetY = getElementYAbs(target.get(0));
+                        setTimeout(function () {
+                            if (Math.abs(targetY - scroller.scrollTop()) < options.menuInitialHashOffsetTolerance) {
+                                scroller.scrollTop(targetY + options.menuScrollOffset);
+                            }
+                        }, 100);
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+
+        // handle links
+        if (0 !== options.menuScrollOffset || options.menuScrollSpeed > 0) {
+            $('a[href^=#]', items).click(function () {
+                var
+                    hash = this.hash.substr(1),
+                    target = $('#' + hash + ', ' + '[name=' + hash + ']'),
+                    currentScrollTop = scroller.scrollTop(),
+                    targetY
+                ;
+
+                // change hash and return to original position
+                scrollWatch.pause();
+                window.location.hash = hash;
+                scroller.scrollTop(currentScrollTop);
+                scrollWatch.resume();
+
+                // get target and its position
+                if (target.length > 0) {
+                    target = target.get(0);
+                    targetY = getElementYAbs(target) + options.menuScrollOffset;
+                } else {
+                    return true;
+                }
+
+                // animate if speed is defined
+                if (options.menuScrollSpeed > 0) {
+
+                    // stop current animation
+                    if (scroller.is(':animated')) {
+                        scroller.stop(true, true);
+                    }
+
+                    // animate
+                    $(scroller).animate({scrollTop: targetY}, options.menuScrollSpeed);
+
+                } else {
+
+                    // instant
+                    scroller.scrollTop(targetY);
+
+                }
+
+                return false;
+
+            });
+        }
+
+        // apply scrollwatch
+        return scrollWatch = $(sections).scrollWatch(options);
+    };
+
+    // defaults
+    $.fn.scrollWatch.defaults = {
+        callback: null,
+        scroller: window,
+        multiMode: false,
+        resolutionMode: 0,
+        topDownWeight: 0,
+        viewMarginTop: 0,
+        viewMarginBottom: 0,
+        focusRatio: 0.38196601125010515,
+        debugFocusLine: false
+    };
+    $.fn.scrollWatchMenu.defaults = {
+        menuActiveClass: 'active',
+        menuItemSelector: '*',
+        menuScrollOffset: 0,
+        menuScrollSpeed: 500,
+        menuInitialHashOffsetTolerance: 40
+    };
+
+}(jQuery);
